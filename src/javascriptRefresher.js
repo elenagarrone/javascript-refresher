c8bdca7ba4fddd2807a0e33d4ff39fab1c9d9147
elena <elena.15@hotmail.it>
2015-02-23 17:52:46 +0000

    test 11 is passing

--- src/javascriptRefresher.js
+++ src/javascriptRefresher.js
@@ -89,3 +89,72 @@ function makeNumberNegative(number) {
   number = -Math.abs(number);
   return number;
 };
+
+// Question 11
+
+function separateArrayIntoEvenAndOddNumbers(array) {
+  var both = [], odd = [], even = [];
+  for(i = 0; i < array.length; i++){
+    array[i] % 2 === 0 ? even.push(array[i]) : odd.push(array[i])  
+  }
+  both.push(even, odd);
+  return both;
+};
+
+// Question 12
+
+function numberOfElementsThatArePalindromes(array) {
+
+};
+
+// Question 13
+
+function shortestWordInArray(array) {
+
+};
+
+// Question 14
+
+function longestWordInArray(array) {
+
+};
+
+// Question 15
+
+function arrayTotal(array) {
+
+};
+
+// Question 16
+
+function doubleArray(array) {
+
+};
+
+// Question 17
+
+function averageOfArray(array) {
+
+};
+
+// Question 18
+
+function removeElementsGreaterThanFive(array) {
+
+};
+
+function isLessThanFive(element) {
+
+};
+
+// Question 19
+
+function convertArrayToObject(array) {
+
+};
+
+// Question 20
+
+function getLettersInArrayOfWords(array) {
+
+};
