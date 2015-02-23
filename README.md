db6638c57650b47774460e8c8d77b181159376a7
elena <elena.15@hotmail.it>
2015-02-23 11:58:12 +0000

    created new project and first test passing

---
+++ README.md
@@ -0,0 +1,25 @@
+Javascript Refresher
+====================
+
+To refresh my knowledge on JavaScript I decided to do the ruby refresher but in JS (taking Andrew Hercules's spec and initial src file).
+
+Technologies:
+------------
+- JavaScript
+- Jasmine
+
+How to test it:
+----------
+
+Clone the directory:
+```shell
+$ git clone https://github.com/elenagarrone/javascript-refresher.git
+```
+Change into it:
+```shell
+$ cd javascript-refresher
+```
+Run:
+```shell
+$ open SpecRunner.html
+```
---
+++ src/javascriptRefresher.js
@@ -0,0 +1,63 @@
+// Question 1
+
+function selectElementsStartingWithE(array) {
+  newArray = [];
+  for (i = 0; i < array.length; i++){
+    if (array[i].charAt(0) === 'e'){ newArray.push(array[i]) }
+  }
+  return newArray;
+};
+
+// Question 2
+
+function selectElementsStartingWithVowel(array) {
+
+};
+
+// Question 3
+
+function removeNullsFromArray(array) {
+
+};
+
+// Question 4
+
+function removeNullsAndFalseFromArray(array) {
+
+};
+
+// Question 5
+
+function reverseEveryElementInArray(array) {
+
+};
+
+// Question 6
+
+function dropFirstThreeElements(array) {
+
+};
+
+// Question 7
+
+function addElementToBeginningOfArray(array, element) {
+
+};
+
+// Question 8
+
+function sortArrayByLastLetterOfEachWord(array) {
+
+};
+
+// Question 9
+
+function returnFirstHalfOfString(string) {
+
+};
+
+// Question 10
+
+function makeNumberNegative(number) {
+  
+};
